09e6bdce6bbe02e7838abf996e428a1676f853fa
stas yudin <info@evp-web.ru>
2023-07-03 15:57:44 +0300

    adaptive header

---
+++ src/js/navBarOpen.js
@@ -0,0 +1,12 @@
+function navBarOpen() {
+	const burger = document.querySelector(".Header__burger");
+	const menu = document.querySelector('.Menu');
+
+	burger.addEventListener('click', (e) => {
+		e.preventDefault();
+		menu.classList.add("Menu_open");
+		burger.classList.add("Header__burger_hidden");
+	})
+}
+
+navBarOpen();
