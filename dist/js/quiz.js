a175233d085878aa2c46da7bf3b951ebdc859855
stas yudin <info@evp-web.ru>
2023-07-04 10:46:37 +0300

    tabs and refactirung js quiz

--- dist/js/quiz.js
+++ dist/js/quiz.js
@@ -1,36 +1,51 @@
-let quizCounter = 1;
-const totalQuestion = 5;
+function quiz() {
+	let quizCounter = 1;
+	const totalQuestion = 5;
 
-const btnNextQuestion = document.querySelector(".Quiz__buttonNextQuestion");
-const viewQuestionNumber = document.querySelector(".Quiz__question_number");
-const progressItems = document.querySelectorAll(".Quiz__ProgressItem");
-const quizQuestions = document.querySelectorAll(".Quiz__question");
-const buttonSend = document.querySelector(".Quiz__buttonSend");
+	const btnNextQuestion = document.querySelector(".Quiz__buttonNextQuestion");
+	const viewQuestionNumber = document.querySelector(".Quiz__question_number");
+	const progressItems = document.querySelectorAll(".Quiz__ProgressItem");
+	const quizQuestions = document.querySelectorAll(".Quiz__question");
+	const buttonSend = document.querySelector(".Quiz__buttonSend");
 
-btnNextQuestion.addEventListener("click", (e) => {
-  e.preventDefault();
-  quizCounter++;
+	btnNextQuestion.addEventListener("click", (e) => {
+		e.preventDefault();
+		quizCounter++;
 
-  // Изменили номер текущего вопроса
-  viewQuestionNumber.innerHTML = quizCounter;
+		changeCurrentNumberQuestion();
+		changeProgressBar();
+		showCurrentQuestion();
+		showCurrentButton();
+	});
 
-  // Изменили прогрессбар
-  progressItems.forEach((item, index) => {
-    if (index < quizCounter) {
-      item.classList.add("Quiz__ProgressItem_fill");
-    }
-  });
+	function changeCurrentNumberQuestion() {
+		viewQuestionNumber.innerHTML = quizCounter;
+	}
 
-  quizQuestions.forEach((question, index) => {
-    if (index + 1 == quizCounter) {
-      question.classList.remove("Quiz__question_hidden");
-    } else {
-      question.classList.add("Quiz__question_hidden");
-    }
-  });
+	function changeProgressBar() {
+		progressItems.forEach((item, index) => {
+			if (index < quizCounter) {
+				item.classList.add("Quiz__ProgressItem_fill");
+			}
+		});
+	}
 
-  if (quizCounter == totalQuestion) {
-    buttonSend.classList.remove("Quiz__button_hidden");
-    btnNextQuestion.classList.add("Quiz__button_hidden");
-  }
-});
+	function showCurrentQuestion() {
+		quizQuestions.forEach((question, index) => {
+			if (index + 1 == quizCounter) {
+				question.classList.remove("Quiz__question_hidden");
+			} else {
+				question.classList.add("Quiz__question_hidden");
+			}
+		});
+	}
+
+	function showCurrentButton() {
+		if (quizCounter == totalQuestion) {
+			buttonSend.classList.remove("Quiz__button_hidden");
+			btnNextQuestion.classList.add("Quiz__button_hidden");
+		}
+	}
+}
+
+quiz();
