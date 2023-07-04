028dd9e5c25539df8154dbf9089ce79c7768fd14
stas yudin <info@evp-web.ru>
2023-07-04 10:49:45 +0300

    pretier

--- src/js/rentHallPlan.js
+++ src/js/rentHallPlan.js
@@ -1,36 +1,35 @@
 function rentHallPlans() {
-	const tabsItems = document.querySelectorAll('.Tabs__Item');
-	const rentHallPlans = document.querySelectorAll('.RentHall__plan');
+  const tabsItems = document.querySelectorAll(".Tabs__Item");
+  const rentHallPlans = document.querySelectorAll(".RentHall__plan");
 
-	tabsItems.forEach(item => {
-		item.addEventListener("click", (e) => {
-			e.preventDefault();
+  tabsItems.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
 
-			activateTab(e.currentTarget);
-			showCurrentHall(item.dataset.choosehall);
+      activateTab(e.currentTarget);
+      showCurrentHall(item.dataset.choosehall);
+    });
+  });
 
-		})
-	});
+  function activateTab(currentTarget) {
+    tabsItems.forEach((tabItem) => {
+      if (tabItem == currentTarget) {
+        tabItem.classList.add("Tabs__Item_active");
+      } else {
+        tabItem.classList.remove("Tabs__Item_active");
+      }
+    });
+  }
 
-	function activateTab(currentTarget) {
-		tabsItems.forEach(tabItem => {
-			if (tabItem == currentTarget) {
-				tabItem.classList.add('Tabs__Item_active');
-			} else {
-				tabItem.classList.remove('Tabs__Item_active');
-			}
-		})
-	}
-
-	function showCurrentHall(hallNumber) {
-		rentHallPlans.forEach(plan => {
-			if (plan.dataset.currenthall == hallNumber) {
-				plan.classList.remove("RentHall__plan_hidden")
-			} else {
-				plan.classList.add("RentHall__plan_hidden")
-			}
-		})
-	}
+  function showCurrentHall(hallNumber) {
+    rentHallPlans.forEach((plan) => {
+      if (plan.dataset.currenthall == hallNumber) {
+        plan.classList.remove("RentHall__plan_hidden");
+      } else {
+        plan.classList.add("RentHall__plan_hidden");
+      }
+    });
+  }
 }
 
 rentHallPlans();
